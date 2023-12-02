92b59415e4f9f7ea2979917a1fb2f4e9f5e27d69
Sven Painer <sven@painer.de>
2023-12-02 14:33:03 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,33 @@
+import java.lang.IllegalArgumentException
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
 }
+
+private val digits = listOf(
+    "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+)
+
+private fun part1(input: List<String>): Int {
+    return input.sumOf<String> { line ->
+        val firstDigit = line.first { it.isDigit() }.digitToInt()
+        val lastDigit = line.last { it.isDigit() }.digitToInt()
+        firstDigit * 10 + lastDigit
+    }
+}
+
+private fun part2(input: List<String>): Int {
+    return input.sumOf { line ->
+        val firstDigit = line.findAnyOf(digits)?.second?.convertName() ?: throw IllegalArgumentException()
+        val lastDigit = line.findLastAnyOf(digits)?.second?.convertName() ?: throw IllegalArgumentException()
+        firstDigit * 10 + lastDigit
+    }
+}
+
+private fun String.convertName(): Int = (digits.indexOf(this) % 9) + 1
